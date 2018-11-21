cfae2d6df2b8c7f73e2459e1f514a5ff9563750a
Anna <44594570+annelize29@users.noreply.github.com>
2018-11-22 00:13:49 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+const cluster = require('cluster');
+const numCPUs = require('os').cpus().length;
+const Database = require('better-sqlite3');
+const db = new Database('bithack.db', {});
+
+if (cluster.isMaster) {
+    for (let i = 0; i < numCPUs; i++) {
+        cluster.fork();
+    }
+
+    Object.keys(cluster.workers).forEach(function (id) {
+        console.log("Worker running with ID : " + cluster.workers[id].process.pid);
+
+        cluster.workers[id].on('message', function(json){
+
+            if (json.type==="hit")
+            {
+                let stmt = db.prepare("INSERT INTO result VALUES (@wallet, @wif, @balance)");
+                stmt.run(json.data);
+            } else
+            {
+                console.log("Received unknown message from worker", JSON.stringify(json));
+            }
+        });
+
+        // cluster.workers[id].send({type:"message", "message" : "Hello"});
+    });
+
+    cluster.on('exit', function (worker, code, signal) {
+        console.log('Worker ' + worker.process.pid + ' has died');
+    });
+} else {
+    const worker = require("./worker");
+    worker.doProcess();
+}
